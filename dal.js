e1c9d17fd71c9e5260f27387dc7306eb6fe6d218
ajdavidson <89638345+ajdavidson@users.noreply.github.com>
2023-02-09 20:31:43 -0700

    Update dal.js

--- dal.js
+++ dal.js
@@ -1,9 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
 //const url = 'mongodb://localhost:27017';
 //const ev = require('dotenv').config()
-//console.log(process.env.MDB_USR)
-//const url = 'mongodb+srv://' + process.env.MDB_USR + ':' + process.env.MDB_PWD + '@' + process.env.MDB_DBN + '.93pyc5t.mongodb.net/?retryWrites=true&w=majority';
-const url = "mongodb+srv://admin:124get@cluster0.93pyc5t.mongodb.net/?retryWrites=true&w=majority";
+console.log(process.env.MDB_USR)
+const url = 'mongodb+srv://' + process.env.MDB_USR + ':' + process.env.MDB_PWD + '@' + process.env.MDB_DBN + '.93pyc5t.mongodb.net/?retryWrites=true&w=majority';
+//const url = "mongodb+srv://admin:124get@cluster0.93pyc5t.mongodb.net/?retryWrites=true&w=majority";
 let db = null;
 
 // connect to mongo
